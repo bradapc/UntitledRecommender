02a3532ef86195ac9b50135ff94a7aa7a8b6e2a4
bradc <brad.cocar@gmail.com>
2025-07-02 19:38:59 -0700

    Added endpoint for viewing a users seen movies

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -16,4 +16,20 @@ const getWatchlistByID = async (req, res) => {
     }
 };
 
-module.exports = {getWatchlistByID};
+const getSeenByID = async (req, res) => {
+    const userId = req.params.userId;
+    try {
+    const userExists = await db.query("SELECT id FROM users WHERE id = $1", [userId]);
+    if (userExists.rowCount === 0) {
+        return res.status(404).json({"message": `User with ID ${userId} does not exist.`});
+    }
+
+    const userSeen = await db.query('SELECT * FROM movies_seen WHERE user_id = $1', [userId]);
+    return res.status(200).json({userId, seen: userSeen.rows});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({"message": "Error when attempting to retrieve from database"});
+    }
+};
+
+module.exports = {getWatchlistByID, getSeenByID};
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 router.get('/:userId/watchlist', usersController.getWatchlistByID);
+router.get('/:userId/seen', usersController.getSeenByID);
 
 module.exports = router;
