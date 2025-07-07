060b6515669ef17b7d49772a68a13b77a8da21f9
bradc <brad.cocar@gmail.com>
2025-07-07 12:30:32 -0700

    Started frontend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,33 @@
+import Header from './Header';
+import {useState, useEffect} from 'react';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
 function App() {
+  const [discover, setDiscover] = useState([]);
+
+  const handleDiscoverRequest = async () => {
+      try {
+        const response = await fetch(`${API_URL}/discover`);
+        console.log(API_URL);
+        const result = await response.json();
+        setDiscover(result);
+      } catch (err) {
+        console.log(err);
+      }
+  };
+
+  useEffect(() => {
+    handleDiscoverRequest();
+  }, [])
+
   return (
     <div className="App">
+        <Header />
+        {discover ? discover.map(movie => (
+            <p>{movie.name}</p>
+        ))
+      : <p>No movies found.</p>}
     </div>
   );
 }
---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <h1>Recommender</h1>
+        <Nav />
+    </div>
+  )
+}
+
+export default Header
