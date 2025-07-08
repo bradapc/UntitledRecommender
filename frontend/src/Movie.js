571056db65ae69684a4ed83d82197c9f9bcd83a7
bradc <brad.cocar@gmail.com>
2025-07-07 19:23:06 -0700

    Formatted discover carousel

--- frontend/src/Movie.js
+++ frontend/src/Movie.js
@@ -1,13 +1,18 @@
 const Movie = ({movie}) => {
   return (
     <div className="Movie">
-        <h2>{movie.title}</h2>
-        <p>{movie.release_date}</p>
-        <p>{movie.overview}</p>
-        <p>{movie.genre_ids.join(', ')}</p>
         <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         alt="Movie Poster"
         className="MoviePoster"></img>
+        <h2 className="movie-title">{movie.title}</h2>
+        <span>{movie.release_date}</span>
+        {movie.overview.length < 250 ? <span>{movie.overview}</span> : (
+          <div>
+            <span>{movie.overview.slice(0, movie.overview.slice(0, 250).lastIndexOf(' '))}...</span>
+            <span className="readMoreText"> read more</span>
+          </div>
+          )}
+        <p>{movie.genre_ids.join(', ')}</p>
     </div>
   )
 }
