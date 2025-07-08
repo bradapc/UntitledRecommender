eddad90b05ce93d10cfef26c705607878a1e5ac0
bradc <brad.cocar@gmail.com>
2025-07-08 09:45:39 -0700

    Updated movie poster style, sizing

--- frontend/src/Movie.js
+++ frontend/src/Movie.js
@@ -1,8 +1,10 @@
 import {useContext} from 'react';
 import { DataContext } from './context/DataContext';
+import {useState} from 'react';
 
 const Movie = ({movie}) => {
   const {genres} = useContext(DataContext);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <div className="Movie">
@@ -11,16 +13,19 @@ const Movie = ({movie}) => {
                 <span className="genreTag">{genres[id]} </span>
             ))}
         </div>
-        <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-        alt="Movie Poster"
-        className="MoviePoster"></img>
+          <div className="MoviePosterWrapper">
+          {!isLoaded && <div className="placeholder-movie-poster">Loading...</div>}
+          <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+          className="MoviePoster"
+          onLoad={() => setIsLoaded(true)}></img>
+        </div>
         <h2 className="movie-title">{movie.title}</h2>
         <span className="movie-release">{movie.release_date}</span>
         {movie.overview.length < 250 ?
         <span className="movie-overview">{movie.overview}</span> : (
           <div className="movie-overview">
-            <span>{movie.overview.slice(0, movie.overview.slice(0, 250).lastIndexOf(' '))}...</span>
-            <span className="readMoreText"> read more</span>
+            <span>{movie.overview.slice(0, movie.overview.slice(0, 250).lastIndexOf(' '))}... </span>
+            <span className="readMoreText">read more</span>
           </div>
           )}
     </div>
