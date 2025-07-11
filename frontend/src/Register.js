a93c73d1735b92197c5c6a429adbe65bb85dbe31
bradc <brad.cocar@gmail.com>
2025-07-11 10:46:39 -0700

    Created registration page style

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,7 +1,20 @@
+import './css/Register.css';
+import {Link} from "react-router-dom";
+
 const Register = () => {
   return (
-    <div>
-      
+    <div className="Register">
+        <form className="RegisterForm">
+            <div className="RegisterTextWrapper">
+                <h3>Register</h3>
+                <p>Register to Discover Great Movies</p>
+            </div>
+            <input placeholder="Username" id="username" type="text" required></input>
+            <input placeholder="Email" id="email" type="email" required></input>
+            <input placeholder="Password" id="password" type="password" required></input>
+            <button type="submit" className="RegisterButton">Register</button>
+            <p>Already have an account? <Link to="/login">Login</Link></p>
+        </form>
     </div>
   )
 }
