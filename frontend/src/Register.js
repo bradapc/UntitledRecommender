b250cbd8c24a4d0a88b308e15ff1bf259e02818a
bradc <brad.cocar@gmail.com>
2025-07-11 11:16:39 -0700

    Added registration call from front end

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,18 +1,55 @@
 import './css/Register.css';
 import {Link} from "react-router-dom";
+import {useContext, useState} from 'react';
+import {DataContext} from './context/DataContext';
+import {useNavigate} from "react-router-dom";
 
 const Register = () => {
+    const {apiUrl} = useContext(DataContext);
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorStatus, setErrorStatus] = useState(null);
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const postOptions = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, email, password})
+        }
+        console.log(JSON.stringify({username, email, password}))
+        const response = await fetch(`${apiUrl}/signup`, postOptions)
+        if (!response.ok) {
+            setErrorStatus(response.status);
+        } else {
+            navigate('/');
+        }
+    };
+
   return (
     <div className="Register">
+        {errorStatus != null && (errorStatus === 409 ? (
+            <div className="ErrorRegister">
+                <span>Username/Email already taken</span>
+            </div>
+            ) : 
+            <div className="ErrorRegister">
+                <span>Registration Error</span>
+            </div>
+        )}
         <form className="RegisterForm">
             <div className="RegisterTextWrapper">
                 <h3>Register</h3>
-                <p>Register to Discover Great Movies</p>
+                <p>Discover Great Movies</p>
             </div>
-            <input placeholder="Username" id="username" type="text" required></input>
-            <input placeholder="Email" id="email" type="email" required></input>
-            <input placeholder="Password" id="password" type="password" required></input>
-            <button type="submit" className="RegisterButton">Register</button>
+            <input placeholder="Username" id="username" type="text" required value={username} onChange={(e) => setUsername(e.target.value)}></input>
+            <input placeholder="Email" id="email" type="email" required value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            <input placeholder="Password" id="password" type="password" required value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <button type="submit" className="RegisterButton" onClick={(e) => handleRegister(e)}>Register</button>
             <p>Already have an account? <Link to="/login">Login</Link></p>
         </form>
     </div>
