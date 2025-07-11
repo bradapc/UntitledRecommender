33e625ccf04b5af0c593dc0fa289b9b47e4961a7
bradc <brad.cocar@gmail.com>
2025-07-11 09:53:15 -0700

    Add nav hover effect

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,15 +1,63 @@
 import './css/Nav.css';
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState([
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+      active: true
+    },
+    {
+      id: 2,
+      path: '/discover',
+      text: 'Discover',
+      active: false
+    },
+    {
+      id: 3,
+      path: '/popular',
+      text: 'Popular',
+      active: false
+    },
+    {
+      id: 4,
+      path: '/top-rated',
+      text: 'Top Rated',
+      active: false
+    },
+    {
+      id: 5,
+      path: '/login',
+      text: 'Login',
+      active: false
+    },
+    {
+      id: 6,
+      path: '/register',
+      text: 'Register',
+      active: false
+    }
+  ]);
+
+  const handleNavClick = (key) => {
+    setActiveNav(activeNav.map(NavElement => {
+      return NavElement.id === key ? {...NavElement, active: true}
+      : {...NavElement, active: false}
+    }));
+  };
+
   return (
     <div className="Nav">
-        <Link to="/" className="NavLink">Home</Link>
-        <Link to="/discover" className="NavLink">Discover</Link>
-        <Link to="/popular" className="NavLink">Popular</Link>
-        <Link to="/top-rated" className="NavLink">Top Rated</Link>
-        <Link to="/login" className="NavLink">Login</Link>
-        <Link to="/register" className="NavLink">Register</Link>
+        {activeNav.map(NavElement => NavElement.active ? (
+          <Link to={NavElement.path} className="NavLink" key={NavElement.id} style={{borderBottom: "4px solid #ffffffff"}}>{NavElement.text}</Link>
+        )
+      : (
+          <Link to={NavElement.path} className="NavLink" key={NavElement.id} onClick={() => handleNavClick(NavElement.id)}>{NavElement.text}</Link>
+      )
+      )}
     </div>
   )
 }
