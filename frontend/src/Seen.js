d954b16fc2698677ce7f2131b88993dc6877051d
bradc <brad.cocar@gmail.com>
2025-07-19 15:34:27 -0700

    Add reviewed status filter, search bar for seen movies

--- frontend/src/Seen.js
+++ frontend/src/Seen.js
@@ -11,16 +11,17 @@ const Seen = () => {
   const {seenList, setSeenList} = useSeen();
   const [filteredSeenList, setFilteredSeenList] = useState(seenList);
   const [filterRating, setFilterRating] = useState('any');
+  const [reviewedStatus, setReviewedStatus] = useState('any');
+  const [filterText, setFilterText] = useState('');
 
   useEffect(() => {
       let filterList;
-      if (filterRating === 'any') {
-        setFilteredSeenList(seenList);
-        return;
-      }
-      filterList = filterRating === 'any' ? filteredSeenList : seenList.filter(movie => Number(movie.rating) === Number(filterRating));
+      filterList = filterRating === 'any' ? seenList : seenList.filter(movie => Number(movie.rating) === Number(filterRating));
+      filterList = reviewedStatus === 'any' ? filterList : reviewedStatus === 'reviewed' ? filterList.filter(movie => movie.review != null) : filterList.filter(movie => movie.review === null);
+      filterList = filterList.filter(movie => movie.title.toLowerCase().includes(filterText.toLowerCase()));
+      console.log(filterList);
       setFilteredSeenList(filterList);
-  }, [filterRating, seenList]);
+  }, [filterRating, seenList, reviewedStatus, filterText]);
 
   useEffect(() => {
     if (!isAuth) {
@@ -34,16 +35,27 @@ const Seen = () => {
       <div className="SeenFilter">
           <form>
             <div className="SeenFilterOptions">
-              <label htmlFor="rating">Rating</label>
-                <select name="rating" id="rating" value={filterRating} onChange={(e) => setFilterRating(e.target.value)}>
+              <div>
+                <label htmlFor="rating">Rating</label>
+                  <select name="rating" id="rating" value={filterRating} onChange={(e) => setFilterRating(e.target.value)}>
+                    <option value="any">Any</option>
+                    <option value="5">★★★★★</option>
+                    <option value="4">★★★★</option>
+                    <option value="3">★★★</option>
+                    <option value="2">★★</option>
+                    <option value="1">★</option>
+                  </select>
+              </div>
+              <div>
+                <label htmlFor="reviewed">Reviewed</label>
+                <select name="reviewed" id="reviewed" value={reviewedStatus} onChange={(e) => setReviewedStatus(e.target.value)}>
                   <option value="any">Any</option>
-                  <option value="5">★★★★★</option>
-                  <option value="4">★★★★</option>
-                  <option value="3">★★★</option>
-                  <option value="2">★★</option>
-                  <option value="1">★</option>
+                  <option value="reviewed">Reviewed</option>
+                  <option value="notreviewed">Not Reviewed</option>
                 </select>
+              </div>
             </div>
+            <input type="text" placeholder="Search movie titles..." value={filterText} onChange={(e) => setFilterText(e.target.value)}></input>
             <div className="SeenFilterSort">
 
             </div>
@@ -53,6 +65,9 @@ const Seen = () => {
           filteredSeenList.map(movie => (
               <SeenMovie key={movie.movie_id} movie={movie} setSeenList={setSeenList} />
           ))
+        )}
+        {filteredSeenList.length == 0 && (
+          <span>Your search yielded no results. Please try again.</span>
         )}
     </div>
   )
