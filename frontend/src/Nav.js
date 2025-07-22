410d69aa1a654cbab80670d880e510e65e63ec9f
bradc <brad.cocar@gmail.com>
2025-07-22 09:57:28 -0700

    Added nav bar menu for user account with dropdown for seen list and watchlist

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,10 +1,15 @@
 import './css/Nav.css';
 import {NavLink} from 'react-router-dom';
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import {DataContext} from './context/DataContext';
 
 const Nav = () => {
   const {isAuth} = useContext(DataContext);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const closeDropdown = () => setIsDropdownOpen(false);
+
+  const toggleDropdown = () => setIsDropdownOpen(prev => !prev);
 
   return (
     <div className="Nav">
@@ -16,7 +21,17 @@ const Nav = () => {
         </div>
         <div className="NavRight">
           {isAuth ? (
-            <NavLink to="/logout" className="NavLink">Logout</NavLink>
+            <div className="NavRightWrapper">
+              <div className="dropdown-user" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+              <span className="NavLink">User</span>
+              {isDropdownOpen && <div className="nav-dropdown-content">
+                  <NavLink to="/watchlist" className="NavLink" onClick={closeDropdown}>Watchlist</NavLink>
+                  <NavLink to="/seen" className="NavLink" onClick={closeDropdown}>Seen</NavLink>
+              </div>
+              }
+              </div>
+              <NavLink to="/logout" className="NavLink">Logout</NavLink>
+            </div>
           ) : (
             <>
               <NavLink to="/login" className={({isActive}) => (isActive ? "NavLink active" : "NavLink")}>Login</NavLink>
